56a5212538059f96933663a801fd88f2f43c635a
Romain <romain.kruszewski@campus-igs-toulouse.fr>
2018-10-25 15:17:59 +0200

    barry header

--- header.php
+++ header.php
@@ -1,14 +1,23 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6">
-            <img src="img/Logo_IGHug.png">
+        <div class="col-md-1 col-lg-1">
+            <img id="logo_header" src="img/Logo_IGHug.png"/>
         </div>
-        <div class="col-md-6">
-            <div class="page-header">
+        <div class="col-md-9 col-lg-9">
+            <div class="page-header text-center">
                 <h1>
-                    IGHub <small>Ceci est le Header</small>
-                    <img src="img/Barry.jpg" height="5%" width="5%">
+                    IGHub
                 </h1>
+                <h2>
+                    Du travail et des calins
+                </h2>
+            </div>
+        </div>
+        <div class="col-md-2 col-lg-2 zoom icone_header">
+            <a href="http://localhost/IGHug/HUB.php"><img src="img/accueil_white-50.png"/></a>
+            <a href="http://localhost/IGHug/Research.php"><img src="img/rechercher_white-48.png"/></a>
+            <a href="http://localhost/IGHug/Event.php"><img src="img/champagne_white-52.png"/></a>
+            <a href="http://localhost/IGHug/Modify.php"><img src="img/contacts_white-50.png"/></a>
             </div>
         </div>
     </div>
