5b17636b8d9797bd3a187df5130c891f6acaaafd
Quentin <quentin.baillau@campus-igs-toulouse.fr>
2018-10-26 17:29:49 +0200

    barry

--- Accueil.php
+++ Accueil.php
@@ -3,25 +3,27 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/Logo_IGHug.png" />
 
     <title>IGHUG</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style_Accueil.css" rel="stylesheet">
 
   </head>
   <body>
     <div class="container-fluid">
-		<div class="row">
-			<div class="col-md-6">
-				<img alt="Logo" src="img\Logo_IGHug.png">
+		<div class="row" id="color_head">
+			<div class="col-md-6 form-inline">
+				<img id="logo" alt="Logo" src="img\Logo_IGHug.png">
+				<p id="desc">Le site de rencontre du campus IGS</p>
 			</div>
 			<div class="col-md-6">
-				<form class="form-inline">
-					<div class="Login">
+				<form class="form-inline" id="login">
+					<div>
 						<input type="email" class="form-control" placeholder="Identifiant">
 					</div>
-					<div class="Login">
+					<div>
 						<input type="password" class="form-control" placeholder="Mot de passe">
 					</div>
 					<div class="Submit">
@@ -32,11 +34,15 @@
 				</form>
 			</div>
 		</div>
+	<hr />
 		<div class="row">
 			<div class="col-md-12">
-				<h1 align="center">Site de rencontre du groupe IGS</h1>
+				<h1 align="center">Ighug te met en relation avec des personnes de ton campus avec qui tu as surement déja échangé un regard</h1>
+				<h2 align="center">Grace à Ighug retrouve cette personne ou découvres en d'autre, pour un verre, un soir ou plus longtemps.</h1>
+				<h3 align="center">Inscris toi</h2>
 			</div>
 		</div>
+<br>
 			<div class="row">
 				<div class="col-md-6">
 					<form role="form" >
@@ -60,11 +66,18 @@
 							<option>IPI</option>
 							<option>ICD</option>
 							<option>ISCPA</option>
+							<option>IGS-RH</option>
+							<option>IGS-Formation continue</option>
+							<option>IGS-Alternance</option>
+							<option>ESAM</option>
 						</select>
-						<br>		
+						<br>
+						<div class="form-group">
+							<input type="email" class="form-control" placeholder="Votre Email">
+						</div>		
 						<div class="Submit">
 							<a href="HUB.html">
-							<button type="Submit" class="btn btn-danger">S'inscrire</button>
+								<button type="Submit" class="btn btn-danger">S'inscrire</button>
 							</a>
 						</div>
 					</form>
@@ -81,7 +94,7 @@
 					<form class="form-inline">
 						<div class="col-md-4">
 							<blockquote class="blockquote">
-								<p class="mb-0">
+								<p>
 									"Grace à Ighug, j'ai pu allié ma passion du dév et de la drague"
 								</p>
 								<cite>- André Rémuzon, IPI</cite>
@@ -89,7 +102,7 @@
 						</div>
 						<div class="col-md-4">
 							<blockquote class="blockquote">
-								<p class="mb-0">
+								<p>
 									"Les beurettes d'IGS n'ont plus eu de secrets pour moi, grace à cet outil"
 								</p>
 								<cite>- Romain Krumescouilles, IPI</cite>
@@ -97,7 +110,7 @@
 						</div>
 						<div class="col-md-4">
 							<blockquote class="blockquote">
-								<p class="mb-0">
+								<p>
 									"Passionné depuis toujours par les buckets de poulet, ce site m'a sauvé la vie en me remettant sur le droit chemin des petites"
 								</p>
 								<cite>- Alexis Chatain, IPI</cite>
