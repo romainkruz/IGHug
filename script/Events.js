be009b43f893123005a0abc027f0f2ae6c46fa5d
Andre <andre.remuzon@campus-igs-toulouse.fr>
2018-11-26 16:21:09 +0100

    azeswxcv

--- script/Events.js
+++ script/Events.js
@@ -1,6 +1,57 @@
+var Participation = {
+	evenements: function() {
+		$('.participation').click(this.click);	
+	},
+	precharger: function() {
+		 var participations = $('.participation');
+		 for (var i = 0; i < participations.length; i++) {
+		 		$.ajax({
+		 			context: participations[i],
+		 			type: "POST",
+		 			url: "view/Participation.php",
+		 			data: "cle=" + encodeURIComponent($(participations[i]).attr('title')),
+		 			success: function(data) {
+		 				var votes = data;
+						if (votes > 0) {
+							$(this).html("Je participe (nombre de participants actuels : " + votes + ")");
+						} else {
+							$(this).html("Je participe (Pas de participant actuellement !)");
+						}
+		 			}
+		 		});
+		 	}
+	},
+	click: function(e) {
+		var element = e.currentTarget;
+		$(element).unbind('click');
+		$(element).click(function() { return false; });
+		$.ajax({
+			context: element,
+			type: "POST",
+			url: "view/Participation.php",
+			data: "vote=&cle=" + encodeURIComponent($(element).attr('title')),
+			success: function(data){
+				$(this).css({'opacity':0});
+				$(this).addClass('participe');
+
+				var votes = data;
+				if (votes > 1) {
+					$(this).html(votes + " personnes participent");
+				} else {
+					$(this).html(votes + " personne participe");
+				}
+
+				$(this).animate({"opacity": "1", "duration": "slow"});
+			},
+			error: function() {
+				alert("Une erreur s'est produite, merci de recharger la page !");
+			}
+		});
+		return false;
+	}
+};
+
 $(document).ready(function(){
-	$('.participe').submit(function(e){
-		e.preventDefault();
-		
-	});
+	Participation.evenements();
+	Participation.precharger();
 });
--- view/ListEvents.php
+++ view/ListEvents.php
@@ -3,13 +3,13 @@
     try {$bdd = new PDO('mysql:host=localhost;dbname=ighug_db', 'root', '',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));}
     catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
 
-    $reponse = $bdd->query('SELECT * FROM events ORDER BY date');
+    $reponse = $bdd->query("SELECT * FROM events ORDER BY date");
 
     while ($donnees = $reponse->fetch()) {
         $today = new DateTime();
         $today = $today->format('Y-m-d');
         if (strtotime($donnees['date']) - strtotime($today) >= 0) {
-            echo '<div class="events_list"><form class="participe" action="view/Participation.php" method="post"><button type="submit">Je participe</button></form><span><strong>Date de l\'évènement :</strong></span>' . ' ' . $donnees['date'] . '</br><span><strong>Heure de l\'évènement : </strong></span>' . $donnees['heure'] . '</br><span><strong>Lieu de l\'évènement : </strong></span>' . $donnees['nom'] . '<p>' . $donnees['description'] . '</p></div>';
+            echo '<div class="events_list"><a href="#" title="' . $donnees['id_events'] . '" class="participation">Je participe</a><span></br><strong>Date de l\'évènement :</strong></span>' . ' ' . $donnees['date'] . '</br><span><strong>Heure de l\'évènement : </strong></span>' . $donnees['heure'] . '</br><span><strong>Lieu de l\'évènement : </strong></span>' . $donnees['nom'] . '<p>' . $donnees['description'] . '</p></div>';
         }
     }
 
