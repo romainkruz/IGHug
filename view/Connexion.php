050bd91adc8a0e822d7b394d77bebd4a21e7192b
Quentin <quentin.baillau@campus-igs-toulouse.fr>
2018-11-30 12:37:58 +0100

    barry

--- view/Connexion.php
+++ view/Connexion.php
@@ -7,8 +7,7 @@
         $Motdepasse = $_POST['Motdepasse'];
 
 // On hash le password
-        $Motdepasse = sha1($Motdepasse);
-        $hash = $Motdepasse;
+        $hash = sha1($Motdepasse);
 
 // On se connecte à la base
 
@@ -20,36 +19,31 @@
 
 //On fait la requéte à la bdd
 
-        $connexion = $bdd->query("SELECT * FROM members WHERE Email = '$email1' AND Login = '$Motdepasse'");
+        $connexion = $bdd->query("SELECT * FROM members WHERE Email = '$email1' AND Login = '$hash'");
         $obj = $connexion->fetch();
-
-// On compare le mdp hashé
-
-        if ($Motdepasse == $hash) {
 
 // On initie la session en marquant l'utilisateur comme Online
 
-            if ($connexion->rowCount() == 1) {
-                $bdd->query("UPDATE members SET Online = '1' WHERE Email = '$email1';");
-                session_start();
-                $_SESSION['email1'] = $_POST['email1'];
-                $_SESSION['motdepasse'] = $_POST['Motdepasse'];
-                $_SESSION['loginok'] = true;
-                $_SESSION['id'] = $obj['ID'];
+        if ($connexion->rowCount() == 1) {
+            $bdd->query("UPDATE members SET Online = '1' WHERE Email = '$email1';");
+            session_start();
+            $_SESSION['email1'] = $_POST['email1'];
+            $_SESSION['motdepasse'] = $hash;
+            $_SESSION['loginok'] = true;
+            $_SESSION['id'] = $obj['ID'];
 
 // On redirige l'utilisateur suivant les données fournis
 
-                if ($_SERVER['SERVER_NAME'] == "localhost") {
-                    header('Location:http://localhost/IGHug/Hub.php');
-                } else {
-                    header('Location:http://' . $_SERVER['SERVER_NAME'] . ':8080/Hub.php');
-                }
+            if ($_SERVER['SERVER_NAME'] == "localhost") {
+                header('Location:http://localhost/IGHug/Hub.php');
             } else {
-                if ($_SERVER['SERVER_NAME'] == "localhost") {
-                    header('Location:http://localhost/IGHug/Accueil.php');
-                } else {
-                    header('Location:http://' . $_SERVER['SERVER_NAME'] . ':8080/Accueil.php');
-                }
+                header('Location:http://' . $_SERVER['SERVER_NAME'] . ':8080/Hub.php');
+            }
+        } else {
+            if ($_SERVER['SERVER_NAME'] == "localhost") {
+                header('Location:http://localhost/IGHug/Accueil.php');
+            } else {
+                header('Location:http://' . $_SERVER['SERVER_NAME'] . ':8080/Accueil.php');
             }
         }
     }
