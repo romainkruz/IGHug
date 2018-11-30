6fbabbd0e7c93c47d328729b83e3b4583587378c
Romain <romain.kruszewski@campus-igs-toulouse.fr>
2018-11-30 12:21:17 +0100

    tamere

--- script/Hub.js
+++ script/Hub.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
 
-    var Members = "";
+    var Members = ""; // Initialisation de la variable à vide
 
-    $("#school_div").children().on("click", function () {
+    $("#school_div").children().on("click", function () { // Création de l'événement sur le clique des enfants de school_div
 
-        var School = $(this).attr('id');
-        if (School == "IGS-Formation-continue"){
+        var School = $(this).attr('id'); // Création de la variable avec l'ID de l'élément cliqué
+        if (School == "IGS-Formation-continue"){ // Condition qui s'applique si l'ID est IGS-Formation.. et formatage de cette dernière pour correspondre à la valeur de la base
             School = "IGS-Formation continue";
         }
-        if (Members == School){
-            $('#Members_by_school').fadeOut("slow");
-            Members = "";
+        if (Members == School){ // Condition qui s'applique si l'élément cliqué est le même que l'élément précedemment cliqué
+            $('#Members_by_school').fadeOut("slow"); // Masquage de la div
+            Members = ""; // Vidage de la variable
         }
         else{
-            Members = School;
-            var url = "view/ListMembers_BySchool.php?school=" + School;
+            Members = School; // Condition qui s'applique si l'élément cliqué n'est pas le même que l'élément précedemment cliqué
+            var url = "view/ListMembers_BySchool.php?school=" + School; // Création de l'url pour préparé la requête AJAX avec l'école en paramétre
 
-            $.ajax({
-                url: url,
-                success: function(ListMembers_BySchool) {
-                    $('#Members_by_school').html(ListMembers_BySchool).hide().fadeIn("slow");
+            $.ajax({ // Créationde la requête AJAX
+                url: url, // Passage de l'URL
+                success: function(ListMembers_BySchool) { // Si la requête à réussi créé la fonction pour écrire dans la div
+                    $('#Members_by_school').html(ListMembers_BySchool).hide().fadeIn("slow"); // Ecriture dans la div avec le html créé par ListMembers_BySchool.php
                 }
             });
         }
@@ -28,37 +28,31 @@ $(document).ready(function () {
 
 function showPopup(ID) {
 
-    var url = "view/OneMember.php?id=" + ID;
-    // ici on insère dans notre page html notre div gris
-    $("#customPopup").before('<div id="grayBack"></div>');
+    var url = "view/OneMember.php?id=" + ID; // Création de l'url pour préparé la requête AJAX avec l'ID du profil en paramétre
 
-    // enfin, on fait apparaître en 300 ms notre div gris de fond, et une fois
-    // son apparition terminée, on fait apparaître en fondu notre popup
-    $("#grayBack").css('opacity', 0).fadeTo(300, 0.5, function () { $("#customPopup").fadeIn(500); });
+    $("#customPopup").before('<div id="grayBack"></div>'); //Insertion dans HUB.php html de la div pour le fond gris
 
-    $.ajax({
-        url: url, // fait bien attention à l'adresse par contre
-        success: function(One_Members) {
-            // contenu_soumettre_tv contient tout
-            // ce que renvoit "soumettre_cv.php" si tu l'appelais en direct
-            $("#customPopupcontent").html(One_Members); // on met le contenu dans le <div class="art">
+    $("#grayBack").css('opacity', 0).fadeTo(300, 0.5, function () { $("#customPopup").fadeIn(500); }); //Apparition du Popup
+
+    $.ajax({ // Créationde la requête AJAX
+        url: url, // Passage de l'URL
+        success: function(One_Members) { // Si la requête à réussi créé la fonction pour écrire dans la div
+            $("#customPopupcontent").html(One_Members); // Ecriture dans la div avec le html créé par One_Members.php
         }
     });
 }
 
 function hidePopup() {
 
-    // on fait disparaître le gris de fond rapidement
-    $("#grayBack").fadeOut('fast', function () { $(this).remove() });
+    $("#grayBack").fadeOut('fast', function () { $(this).remove() }); // Disparition du fond gris
 
-    // on fait disparaître le popup à la même vitesse
-    $("#customPopup").fadeOut('fast', function () { $(this).hide() });
+    $("#customPopup").fadeOut('fast', function () { $(this).hide() }); // Disparition de la div avec l'ID customPopup
 
 }
 
 function showmail(mail) {
 
-    $("#onemember_mail_button").hide();
-    $("#onemember_mail").html(mail);
+    $("#onemember_mail_button").hide(); // Disparition du bouton
+    $("#onemember_mail").html(mail); // Apparition de l'adresse mail
 
 }
